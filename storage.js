79070831fdd201d44df7cb224ca87e035af11608
Jeison Higuita <jeisonhs93@gmail.com>
2019-10-20 22:54:17 +0200

    read/write json file

--- storage.js
+++ storage.js
@@ -1,9 +1,25 @@
+const fs = require("fs");
+
+const readData = () =>
+  fs.readFile(".data.json", (err, data) => {
+    if (err) throw err;
+    return JSON.parse(data);
+  });
+
+const writeData = commands => {
+  let data = JSON.stringify(commands);
+  fs.writeFileSync(".data.json", data);
+  return "done";
+};
+
 function Storage(initial = {}) {
   this.commands = initial;
+  writeData(initial);
   this.setCommands = commands => {
     this.commands = commands;
+    writeData(commands);
   };
-  this.getCommands = () => this.commands;
+  this.getCommands = readData;
 }
 
 module.exports = Storage;
