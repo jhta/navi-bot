5ae36173850de2684ceaa6896aca0eab757bf062
Jeison Higuita <jeisonhs93@gmail.com>
2019-10-22 23:33:23 +0200

    put everything together

--- bot.js
+++ bot.js
@@ -23,20 +23,6 @@ const controller = new Botkit({
 
 // loadRoutes(controller);
 
-let storage = null;
-
-controller.ready(async () => {
-  // load routes
-  // controller.loadModules(__dirname + "/routes");
-
-  const commands = await fetchCommandsFromCms();
-  storage = await createStorage(commands);
-
-  await Skills.help(controller, { storage });
-  Skills.hello(controller);
-  await cms(controller, { storage });
-});
-
 function findDiff(str1, str2) {
   const s1 = JSON.stringify(str1);
   const s2 = JSON.stringify(str2);
@@ -47,48 +33,62 @@ function findDiff(str1, str2) {
   return diff;
 }
 
-controller.hears("update_cms", "direct_message", async (bot, message) => {
-  try {
-    const oldCommands = await storage.getCommands();
+let storage = null;
+
+controller.ready(async () => {
+  // load routes
+  // controller.loadModules(__dirname + "/routes");
+
+  const initCommands = await fetchCommandsFromCms();
+  storage = await createStorage(initCommands);
+
+  await Skills.help(controller, { storage });
+  Skills.hello(controller);
+  await cms(controller, { storage });
+
+  controller.hears("update_cms", "direct_message", async (bot, message) => {
+    try {
+      const oldCommands = await storage.getCommands();
+      const newCommands = await fetchCommandsFromCms();
+      const diff = findDiff(oldCommands, newCommands);
+      await storage.setCommands(newCommands);
+
+      const testCommands = await storage.getCommands();
+      await bot.reply(message, "commnads updated. Check:");
+      await bot.reply(message, `${JSON.stringify(newCommands)}`);
+      await bot.reply(message, `diff: ${JSON.stringify(diff)}`);
+    } catch (error) {
+      bot.reply(message, `error reloading: ${error.message}`);
+    }
+  });
+
+  controller.hears("debug", "direct_message", async (bot, message) => {
+    const commands = await storage.getCommands();
+    bot.reply(message, JSON.stringify(commands));
+  });
+
+  controller.webserver.get("/", (req, res) => {
+    res.send(`This app is running Botkit ${controller.version}.`);
+  });
+
+  controller.webserver.get("/install", (req, res) => {
+    res.redirect(controller.adapter.getInstallLink());
+  });
+
+  controller.webserver.get("/install/auth", async (req, res) => {
+    try {
+      await controller.adapter.validateOauthCode(req.query.code);
+      res.json("Success! Bot installed.");
+    } catch (err) {
+      console.error("OAUTH ERROR:", err);
+      res.status(401);
+      res.send(err.message);
+    }
+  });
+
+  controller.webserver.post("/update_cms", async (req, res) => {
     const newCommands = await fetchCommandsFromCms();
-    const diff = findDiff(oldCommands, newCommands);
     await storage.setCommands(newCommands);
-
-    const testCommands = await storage.getCommands();
-    await bot.reply(message, "commnads updated. Check:");
-    await bot.reply(message, `${JSON.stringify(newCommands)}`);
-    await bot.reply(message, `diff: ${JSON.stringify(diff)}`);
-  } catch (error) {
-    bot.reply(message, `error reloading: ${error.message}`);
-  }
-});
-
-controller.hears("debug", "direct_message", async (bot, message) => {
-  const commands = await storage.getCommands();
-  bot.reply(message, JSON.stringify(commands));
-});
-
-controller.webserver.get("/", (req, res) => {
-  res.send(`This app is running Botkit ${controller.version}.`);
-});
-
-controller.webserver.get("/install", (req, res) => {
-  res.redirect(controller.adapter.getInstallLink());
-});
-
-controller.webserver.get("/install/auth", async (req, res) => {
-  try {
-    await controller.adapter.validateOauthCode(req.query.code);
-    res.json("Success! Bot installed.");
-  } catch (err) {
-    console.error("OAUTH ERROR:", err);
-    res.status(401);
-    res.send(err.message);
-  }
-});
-
-controller.webserver.post("/update_cms", async (req, res) => {
-  const newCommands = await fetchCommandsFromCms();
-  await storage.setCommands(newCommands);
-  res.json("storage updated :) ");
+    res.json("storage updated :) ");
+  });
 });
